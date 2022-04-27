a96ada96fa8de8e9124c8ad3588188ff69612471
Your Name <you@example.com>
2022-04-27 03:24:28 +0100

    added psuedo code for start button click

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,24 @@ const startButton = document.getElementById("start-btn");
 //target banner section
 const banner = document.getElementById("banner");
 
+//function to render question
+const renderQuestion = () => {
+  console.log("render question");
+};
+//function to banner section
+const removeBanner = () => {
+  console.log("remove banner");
+};
+
 //declare the event handler function for start button click
 const onStartButtonClick = () => {
   console.log("start button clicked");
+
+  //remove banner section
+  removeBanner();
+
+  //render the question
+  renderQuestion();
 };
 // add event listener to start button
 
