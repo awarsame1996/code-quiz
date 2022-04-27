fae6adb3c66d31230179a0425e8cba147409651a
Your Name <you@example.com>
2022-04-27 05:23:44 +0100

    array for all question and options created and

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,53 @@ const banner = document.getElementById("banner");
 // target main section
 const main = document.getElementById("main");
 
+// current question index
+let questionIndex = 0;
+
+// all options
+const options = ["answer 1", "answer 2", "answer 3", "answer 4"];
+
+//all questions array
+const questions = [
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+  {
+    text: "question: ",
+    options,
+  },
+];
+
 //function to render question
 const renderQuestion = () => {
   console.log("render question");
+  //get current question
+  const currentQuestion = questions[questionIndex];
   // create section
   const section = document.createElement("section");
   section.setAttribute("class", "content-section question-section");
@@ -16,7 +60,7 @@ const renderQuestion = () => {
   const h2 = document.createElement("h2");
   h2.setAttribute("class", "content-section-title");
   //TODO: should render specific to the question (dynamic)
-  h2.textContent = "question x: something will go here";
+  h2.textContent = `${questionIndex + 1}. ${currentQuestion.text}`;
   // creat ul and append 4 li
   const ul = document.createElement("ul");
   ul.setAttribute("class", "feedback-list");
@@ -24,14 +68,17 @@ const renderQuestion = () => {
   // call function to create and append list items to ul****
   const li1 = document.createElement("li");
   li1.setAttribute("class", "list-item");
-  li1.textContent = "answer 1";
+  li1.textContent = currentQuestion.options[0];
   const li2 = document.createElement("li");
   li2.setAttribute("class", "list-item");
-  li2.textContent = "answer 2";
+  li2.textContent = currentQuestion.options[1];
   const li3 = document.createElement("li");
   li3.setAttribute("class", "list-item");
-  li3.textContent = "answer 3";
-  ul.append(li1, li2, li3);
+  li3.textContent = currentQuestion.options[2];
+  const li4 = document.createElement("li");
+  li4.setAttribute("class", "list-item");
+  li4.textContent = currentQuestion.options[3];
+  ul.append(li1, li2, li3, li4);
   // create div and append button
   const div = document.createElement("div");
   div.setAttribute("class", "btn-control");
