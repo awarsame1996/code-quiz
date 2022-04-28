21bb0bfbcc05030d610f24996ac4a9804781dc75
Your Name <you@example.com>
2022-04-28 18:26:52 +0100

    function to advance to next question added

--- assets/js/index.js
+++ assets/js/index.js
@@ -47,7 +47,68 @@ const questions = [
     options,
   },
 ];
+//function to banner section
+const removeBanner = () => {
+  console.log("remove banner");
+  banner.remove();
+};
+// function to remover question section
+const removeQuestion = () => {
+  console.log("remove question");
+  document.getElementById("question-container").remove();
+};
+/*declare the event handler function for next button click
+const onNextButtonClick = () => {
+  console.log("next button clicked");
+};*/
+// event handler function to handle click events in question section
+const handleOptionClick = (event) => {
+  console.log("clicked somewhere in question section");
+  //get current target
+  const currentTarget = event.currentTarget;
+  // get target
+  const target = event.target;
+  //check if click originates from li
+  // check if target element is li element
+  if (target.tagName === "LI") {
+    //get the option the user clicked on
+    const value = target.getAttribute("data-value");
+    console.log(value);
+    //get the question the user answered
+    const question = questions[questionIndex].text;
+    console.log(question);
+    //build an answer object that contains question and answer
+    const answer = {
+      question,
+      value,
+    };
 
+    //TODO: store answer in local storage
+    console.log(answer);
+    //remove question
+    removeQuestion();
+    //go to next question if not the last question
+    if (questionIndex < questions.length - 1) {
+      //go to next question if not last question
+      // increment the quest by 1
+      questionIndex += 1;
+      renderQuestion();
+    } else {
+      // if  last question then render results and form
+
+      renderResults();
+      renderForm();
+    }
+  }
+};
+//function to render results
+const renderResults = () => {
+  console.log("renderResults");
+};
+//function to render form
+const renderForm = () => {
+  console.log("renderForm");
+};
 //function to render question
 const renderQuestion = () => {
   console.log("render question");
@@ -56,10 +117,11 @@ const renderQuestion = () => {
   // create section
   const section = document.createElement("section");
   section.setAttribute("class", "content-section question-section");
+  section.setAttribute("id", "question-container");
   // creat h2
   const h2 = document.createElement("h2");
   h2.setAttribute("class", "content-section-title");
-  //TODO: should render specific to the question (dynamic)
+  // specific to the question (dynamic)
   h2.textContent = `${questionIndex + 1}. ${currentQuestion.text}`;
   // creat ul and append 4 li
   const ul = document.createElement("ul");
@@ -68,38 +130,39 @@ const renderQuestion = () => {
   // call function to create and append list items to ul****
   const li1 = document.createElement("li");
   li1.setAttribute("class", "list-item");
+  li1.setAttribute("data-value", currentQuestion.options[0]);
   li1.textContent = currentQuestion.options[0];
   const li2 = document.createElement("li");
   li2.setAttribute("class", "list-item");
+  li2.setAttribute("data-value", currentQuestion.options[1]);
   li2.textContent = currentQuestion.options[1];
   const li3 = document.createElement("li");
   li3.setAttribute("class", "list-item");
+  li3.setAttribute("data-value", currentQuestion.options[2]);
   li3.textContent = currentQuestion.options[2];
   const li4 = document.createElement("li");
   li4.setAttribute("class", "list-item");
+  li4.setAttribute("data-value", currentQuestion.options[3]);
   li4.textContent = currentQuestion.options[3];
   ul.append(li1, li2, li3, li4);
   // create div and append button
   const div = document.createElement("div");
   div.setAttribute("class", "btn-control");
 
-  // create button
+  /*// create button
   const button = document.createElement("button");
   button.setAttribute("class", "btn");
   button.setAttribute("id", "next-btn");
   button.textContent = "Next";
-  div.append(button);
+  div.append(button);*/
 
   //append h2, ul and div to section
-  section.append(h2, ul, button);
+  section.append(h2, ul);
 
   // append question section to main element
   main.append(section);
-};
-//function to banner section
-const removeBanner = () => {
-  console.log("remove banner");
-  banner.remove();
+  //add event listener on question section
+  section.addEventListener("click", handleOptionClick);
 };
 
 //declare the event handler function for start button click
@@ -112,6 +175,6 @@ const onStartButtonClick = () => {
   //render the question
   renderQuestion();
 };
-// add event listener to start button
 
+// add event listener to next button
 startButton.addEventListener("click", onStartButtonClick);
